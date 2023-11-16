80d27db55c3d3ce0e72643535778dd6014be219c
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2023-11-16 15:38:24 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-let icons = document.getElementById("icons");
+let containerLinhas = document.getElementById("containerLinhas");
+let columNav = document.getElementById("columNav");
 
-icons.addEventListener("click", function () {
-  if (!this.classList.contains("is-open")) {
-    this.classList.remove("is-closed");
-    this.classList.add("is-open");
-  } else if (this.classList.contains("is-open")) {
-    this.classList.remove("is-open");
-    this.classList.add("is-closed");
+// columNav.addEventListener("click", function() {
+//   this.classList.toggle("open")
+// })
+
+containerLinhas.addEventListener("click", function() {
+  this.classList.toggle("openMenu");
+  if (this.classList.contains("openMenu")) {
+    // columNav.style.display = "block";
+    // columNav.style.opacity = "1";
+    columNav.style.animation = "faded 500ms ease-in-out forwards"
+  } else {
+    // columNav.style.display = "none";
+    // columNav.style.opacity = "0";
+    columNav.style.animation = "fadedOut 500ms ease-in-out forwards"
   }
 })
