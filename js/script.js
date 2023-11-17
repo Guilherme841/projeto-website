94108871597abb95675e11c8872b2763ab7eb24a
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2023-11-16 22:01:05 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,11 @@
 let containerLinhas = document.getElementById("containerLinhas");
 let columNav = document.getElementById("columNav");
 
-// columNav.addEventListener("click", function() {
-//   this.classList.toggle("open")
-// })
-
-containerLinhas.addEventListener("click", function() {
+containerLinhas.addEventListener("click", function () {
   this.classList.toggle("openMenu");
   if (this.classList.contains("openMenu")) {
-    // columNav.style.display = "block";
-    // columNav.style.opacity = "1";
     columNav.style.animation = "faded 500ms ease-in-out forwards"
   } else {
-    // columNav.style.display = "none";
-    // columNav.style.opacity = "0";
     columNav.style.animation = "fadedOut 500ms ease-in-out forwards"
   }
 })
