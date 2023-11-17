e91ba71d370b825ecc9da57b3df61b10083d4e48
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2023-11-17 18:44:46 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 let containerLinhas = document.getElementById("containerLinhas");
 let columNav = document.getElementById("columNav");
+let containerOpAll = document.querySelectorAll(".containerOpAll");
+let containerOpAllArr = Array.from(containerOpAll);
+
+for (const elemento of containerOpAllArr) {
+  elemento.addEventListener("click", function () {
+    if (this.classList.contains("openOp")) {
+      this.classList.remove("openOp");
+    } else {
+      for (const e of containerOpAllArr) {
+        e.classList.remove("openOp");
+      }
+      this.classList.add("openOp");
+    }
+  })
+}
 
 containerLinhas.addEventListener("click", function () {
   this.classList.toggle("openMenu");
   if (this.classList.contains("openMenu")) {
-    columNav.style.animation = "faded 500ms ease-in-out forwards"
+    columNav.style.animation = "faded 500ms ease-in-out forwards";
   } else {
-    columNav.style.animation = "fadedOut 500ms ease-in-out forwards"
+    columNav.style.animation = "fadedOut 500ms ease-in-out forwards";
   }
 })
