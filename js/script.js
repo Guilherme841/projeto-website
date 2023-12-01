78a88c556543641351e864624a13a36f7bfbe8a3
Guilherme Oliveira <Guilherme.ismael777@gmail.com>
2023-12-01 17:18:16 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-let containerLinhas = document.getElementById("containerLinhas").addEventListener("click", function() {
-  let columNav = document.getElementById("columNav");
-  this.classList.toggle("openMenu");
-  if (this.classList.contains("openMenu")) {
-    columNav.style.animation = "faded 500ms ease-in-out forwards";
-  } else {
-    columNav.style.animation = "fadedOut 500ms ease-in-out forwards";
-  }
-});
+let containerLinhas = document
+  .getElementById("containerLinhas")
+  .addEventListener("click", function () {
+    let columNav = document.getElementById("columNav");
+    this.classList.toggle("openMenu");
+    if (this.classList.contains("openMenu")) {
+      columNav.style.animation = "faded 500ms ease-in-out forwards";
+    } else {
+      columNav.style.animation = "fadedOut 500ms ease-in-out forwards";
+    }
+  });
 
 let containerMais = document
   .querySelectorAll(".containerMais")
@@ -35,15 +37,17 @@ function linhaFadedOut(event) {
   event.target.nextElementSibling.classList.remove("onMouseA");
 }
 
-let spanMais = document.querySelectorAll(".span-mais").forEach(function(element) {
-  element.addEventListener("click", function() {
-    let containerInfo = element.parentNode;
-    let elementThis = this;
-    containerInfo.classList.toggle("open");
-    if (containerInfo.classList.contains("open")) {
-      elementThis.innerHTML = "-"
-    } else {
-      elementThis.innerHTML = "+"
-    }
-  })
-})
+let spanMais = document
+  .querySelectorAll(".span-mais")
+  .forEach(function (element) {
+    element.addEventListener("click", function () {
+      let containerInfo = element.parentNode;
+      let elementThis = this;
+      containerInfo.classList.toggle("open");
+      if (containerInfo.classList.contains("open")) {
+        elementThis.innerHTML = "-";
+      } else {
+        elementThis.innerHTML = "+";
+      }
+    });
+  });
